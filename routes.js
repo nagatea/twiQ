0de8cd3b0ed4fac4e7066a7a20677cf154f82652
nagatech <cheesecakebot@yahoo.co.jp>
2018-06-06 05:31:32 +0900

    home_timeline取得

--- routes.js
+++ routes.js
@@ -1,15 +1,18 @@
 let configRoutes
+const twit = require('twitter')
+const CONSUMER_KEY = 'BpmL1N0nvXL1Bclo2PmTsbxfR'
+const CONSUMER_SECRET = 'JfkSqgSzjnXTAWocpnHwqrW3GUXynKb1BEKB6pXFaORBtwuzUx'
 
 configRoutes = function(app, server, passport) {
   app.get('/secret', function(req, res) {
     if (passport.session && passport.session.id) {
-      console.log(req.session.passport.user)
+      //console.log(req.session.passport.user)
       res.send('Success! Welcome ' + req.session.passport.user.displayName + '!')
     } else {
       res.send('Faild...')
     }
   })
-  
+
   app.get('/', function (req, res) {
     res.send('Hello World!')
   })
@@ -23,6 +26,34 @@ configRoutes = function(app, server, passport) {
   })
 
   
+  app.all('/api/*', function(req, res, next) {
+    res.contentType('json')
+    res.header('Access-Control-Allow-Origin', '*')
+    if (passport.session && passport.session.id) {
+      next()
+    } else {
+      res.send('Faild...')
+    }
+  })
+  
+
+  app.get('/api/home_timeline', function(req, res) {
+    const client = new twit({
+      consumer_key: CONSUMER_KEY,
+      consumer_secret: CONSUMER_SECRET,
+      access_token_key: req.session.passport.user.twitter_token,
+      access_token_secret: req.session.passport.user.twitter_token_secret
+    })
+
+    client.get('/statuses/home_timeline.json', {include_entities:true}, function(err, tweets, response) {
+      if (!err) {
+        //console.log(tweets)
+        res.send(tweets)
+      } else {
+        console.log(err)
+      }
+    })
+  })
 }
 
 module.exports = {configRoutes: configRoutes}
